c1b44198c5b62e881a54192b949888476d86d14b
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2024-09-20 12:28:48 +0100

    create a toggle button for mobile apps and websites projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import photography from "../assets/portfolio/shotby.png";
 import musicReview from "../assets/portfolio/musicReview.jpg";
 import cameron from "../assets/portfolio/cameron4.jpg";
 import movies from "../assets/portfolio/movies.jpeg";
 import skater from "../assets/portfolio/e-commerce.jpg";
 import ieuan from "../assets/portfolio/ieuan.jpg";
-// import bnb from "../assets/portfolio/bnb.jpg";
-// import musicAPI from "../assets/portfolio/musicAPI.jpg";
-//import drone from "../assets/portfolio/drone.jpg";
-// import weather from "../assets/portfolio/weather.jpg";
+import bnb from "../assets/portfolio/bnb.jpg";
+import musicAPI from "../assets/portfolio/musicAPI.jpg";
+import drone from "../assets/portfolio/drone.jpg";
+import weather from "../assets/portfolio/weather.jpg";
 
 const Portfolio = () => {
+  const [showWebProjects, setWebProjects] = useState(true);
+
   const portfolios = [
     {
       id: 1,
@@ -69,7 +71,7 @@ const Portfolio = () => {
       ),
       src: movies,
       href: "https://github.com/tgulu/Show-Time",
-      sourceCode: "https://show-time-movies.netlify.app/" ,
+      sourceCode: "https://show-time-movies.netlify.app/",
       style: "shadow-green-300",
     },
     {
@@ -84,31 +86,33 @@ const Portfolio = () => {
       sourceCode: "https://github.com/tgulu/Albums",
       style: "shadow-orange-400",
     },
+  ];
 
-    // {
-    //   id: 5,
-    //   child: (
-    //     <>
-    //       <span className="text-sm">Hestia Haven</span>
-    //     </>
-    //   ),
-    //   src: bnb,
-    //   href: "https://github.com/tgulu/Hestia-Haven",
-    //   sourceCode: " https://tgulu.github.io/Hestia-Haven/",
-    //   style: "shadow-pink-400",
-    // },
-    // {
-    //   id: 6,
-    //   child: (
-    //     <>
-    //       <span className="text-sm">Weather Website</span>
-    //     </>
-    //   ),
-    //   src: weather,
-    //   href: "https://github.com/tgulu/Weather",
-    //   sourceCode: "https://tgulu.github.io/Weather/",
-    //   style: "shadow-cyan-400",
-    // },
+  const android = [
+    {
+      id: 1,
+      child: (
+        <>
+          <span className="text-sm">Hestia Haven</span>
+        </>
+      ),
+      src: bnb,
+      href: "https://github.com/tgulu/Hestia-Haven",
+      sourceCode: " https://tgulu.github.io/Hestia-Haven/",
+      style: "shadow-pink-400",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          <span className="text-sm">Weather Website</span>
+        </>
+      ),
+      src: weather,
+      href: "https://github.com/tgulu/Weather",
+      sourceCode: "https://tgulu.github.io/Weather/",
+      style: "shadow-cyan-400",
+    },
 
     // {
     //   id: 6,
@@ -123,34 +127,34 @@ const Portfolio = () => {
     //   style: "shadow-white",
     // },
 
-    // {
-    //   id: 7,
-    //   child: (
-    //     <>
-    //       <span className="text-sm">Android Music Display </span>
-    //     </>
-    //   ),
-    //   src: musicAPI,
-    //   href: "https://github.com/tgulu/AppleMusic",
-    //   sourceCode: "https://github.com/tgulu/AppleMusic",
-    //   style: "shadow-pink-400",
-    // },
+    {
+      id: 2,
+      child: (
+        <>
+          <span className="text-sm">Android Music Display </span>
+        </>
+      ),
+      src: musicAPI,
+      href: "https://github.com/tgulu/AppleMusic",
+      sourceCode: "https://github.com/tgulu/AppleMusic",
+      style: "shadow-pink-400",
+    },
+
+    {
+      id: 3,
+      child: (
+        <>
+          <span className="text-sm">Drone</span>
+        </>
+      ),
+      src: drone,
+      href: "https://github.com/tgulu/Drone",
+      sourceCode: "https://github.com/tgulu/Drone",
+      style: "shadow-pink-600",
+    },
 
     // {
-    //   id: 8,
-    //   child: (
-    //     <>
-    //       <span className="text-sm">Drone</span>
-    //     </>
-    //   ),
-    //   src: drone,
-    //   href: "https://github.com/tgulu/Drone",
-    //   sourceCode: "https://github.com/tgulu/Drone",
-    //   style: "shadow-pink-600",
-    // },
-
-    // {
-    //   id: 9,
+    //   id: 4,
     //   child: (
     //     <>
     //       <span className="text-sm">Android Messenger</span>
@@ -168,6 +172,13 @@ const Portfolio = () => {
       name="portfolio"
       className="bg-gradient-to-b from-black to-gray-800  w-full text-white md:h-screen"
     >
+      {/* Button to toggle between web-projects and android  */}
+      <button
+        className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-all"
+        onClick={() => setWebProjects(!showWebProjects)}
+      >
+        {showWebProjects ? "Web Projects" : "Android"}
+      </button>
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="">
           <p className="pt-20 font-float mt-0 mb-4 text-4xl ">
@@ -181,7 +192,7 @@ const Portfolio = () => {
         >
           {portfolios.map(({ id, child, src, href, style, sourceCode }) => (
             <div key={id} className={`shadow-md  rounded-lg ${style}`}>
-              <a href={sourceCode} target="_blank" rel="noreferrer">
+              <a href={sourceCode} target="_blank" rel="noopener noreferrer">
                 <img
                   src={src}
                   alt=""
@@ -190,7 +201,7 @@ const Portfolio = () => {
               </a>
               <div className="flex items-center justify-center">
                 <button className="w-1/8 px-2 justify-center py-1/8 m-4 duration-200 hover:scale-110">
-                  <a href={href} target="_blank" rel="noreferrer">
+                  <a href={href} target="_blank" rel="noopener noreferrer">
                     Code: {child}
                   </a>
                 </button>
