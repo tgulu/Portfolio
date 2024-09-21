630341416915df191ffbbd39f3f9fcabaf023d3b
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2024-09-21 18:10:44 +0100

    move title and text to center for mobile screens

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,16 +10,20 @@ const Home = () => {
     >
       <div
         name="bio"
-        className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-0.5  md:flex-row"
+        className="max-w-screen-lg mx-auto flex flex-col relative items-center justify-center h-full px-0.5 md:flex-row"
       >
-        <div className="flex flex-col justify-center w-auto h-full">
-          <h5 className="lg:flex flex-col text-4xl max-w-md py-8 sm:text-6xl font-float text-black">
+        {/* <h6>Hi, my name is</h6>
+        <h1></h1> */}
+        <div className="flex flex-col lg:mr-12 justify-center items-center sm:items-start w-auto  h-full ">
+          <h1 className="text-5xl lg:text-7xl font-float text-black text-center sm:text-left">
             Software Developer
-          </h5>
-          <p className="text-black text-base max-w-md">
+          </h1>
+
+          <p className="text-black text-base sm:absolute sm:top-40 max-w-md items-center sm:items-start w-auto sm:h-96 ">
             Driven software developer with a passion for crafting visually
             engaging and user-friendly web experiences and mobile applications
           </p>
+
           <div>
             <Link
               to="portfolio"
@@ -37,7 +41,7 @@ const Home = () => {
           <img
             src={Profile}
             alt="profile pic"
-            className=" rounded-3xl mx-auto max-w-sm max-h-md w-56 md:h-full border-2 border-[#0c0a09] "
+            className=" justify-center relative -z-0 bottom-56 items-centers rounded-3xl mx-auto max-w-sm max-h-md w-56 md:h-full border-2 border-[#0c0a09] "
           />
         </div>
       </div>
