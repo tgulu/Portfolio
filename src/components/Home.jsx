2e8e43da6ac3a739956b9a9f5c65826a0938a909
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2024-09-21 18:33:02 +0100

    move title to the center for mobile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,11 +15,11 @@ const Home = () => {
         {/* <h6>Hi, my name is</h6>
         <h1></h1> */}
         <div className="flex flex-col lg:mr-12 justify-center items-center sm:items-start w-auto  h-full ">
-          <h1 className="text-5xl lg:text-7xl font-float text-black text-center sm:text-left">
+          <h1 className="text-5xl relative bottom-10 lg:bottom-32 lg:text-7xl font-thin font-float text-black text-center sm:text-left">
             Software Developer
           </h1>
 
-          <p className="text-black text-base sm:absolute sm:top-40 max-w-md items-center sm:items-start w-auto sm:h-96 ">
+          <p className="text-black sm:absolute sm:top-40  items-center sm:items-start w-96 sm:h-96 relative top-96 lg:top-72 text-2xl font-thin ">
             Driven software developer with a passion for crafting visually
             engaging and user-friendly web experiences and mobile applications
           </p>
@@ -30,7 +30,7 @@ const Home = () => {
               //    change to projects when you re-factor portfolio page  to="projects"
               smooth
               duration={500}
-              className="text-white w-fit h-10 px-4 py-3 my-6 flex items-center rounded-md bg-gradient-to-r from-[#020617] to-[#0a0a0a] hover:scale-110 cursor-pointer group"
+              className="text-white w-fit h-10 px-4 py-3 my-6 flex items-center rounded-md relative top-72 lg:top-10 bg-gradient-to-r from-[#020617] to-[#0a0a0a] hover:scale-110 cursor-pointer group"
             >
               Projects
             </Link>
@@ -41,7 +41,7 @@ const Home = () => {
           <img
             src={Profile}
             alt="profile pic"
-            className=" justify-center relative -z-0 bottom-56 items-centers rounded-3xl mx-auto max-w-sm max-h-md w-56 md:h-full border-2 border-[#0c0a09] "
+            className=" justify-center relative bottom-96 lg:bottom-9 items-centers rounded-2xl mx-auto max-w-sm max-h-md w-64 md:h-full border-2 border-[#0c0a09] "
           />
         </div>
       </div>
