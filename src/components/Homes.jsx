791e9f070207219c031ea7784c95cb5bf3dbc5dd
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2024-09-22 22:48:15 +0100

    add gap between tech stacks for mobile

--- src/components/Homes.jsx
+++ src/components/Homes.jsx
@@ -5,6 +5,27 @@ import { VscGithub } from "react-icons/vsc";
 import { SlSocialLinkedin } from "react-icons/sl";
 
 const Homes = () => {
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          <SlSocialLinkedin size={30} />
+        </>
+      ),
+      href: "https://linkedin.com/in/thabo-gulu",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          <VscGithub size={30} />
+        </>
+      ),
+      href: "https://github.com/tgulu",
+    },
+  ];
+
   return (
     <div
       name="home"
@@ -12,12 +33,12 @@ const Homes = () => {
     >
       <div
         name="bio"
-        className="flex lg:max-h-screen max-w-screen-xl mx-auto items-center justify-center h-screen border-4 border-green-700"
+        className="flex lg:max-h-screen max-w-screen-xl mx-auto items-center justify-center h-screen "
       >
         {/* Flex container to align title, bio, and image */}
-        <div className="flex flex-col-reverse md:flex-row gap-4 md:gap-0 border-red-600 border-4 items-center md:items-start  md:justify-between w-full mt-14  p-2 md:p-20">
+        <div className="flex flex-col-reverse md:flex-row gap-4 md:gap-0 items-center md:items-start  md:justify-between w-full mt-14  p-2 md:p-20">
           {/* Left section: Title and Bio */}
-          <div className="flex-row justify-start border-4 border-orange-400  max-w p-5">
+          <div className="flex-row justify-start  max-w p-5">
             <h1 className="text-5xl lg:text-7xl font-bold pb-2 font-float text-black sm:text-left mb-5">
               Software Developer
             </h1>
@@ -27,8 +48,37 @@ const Homes = () => {
             </p>
             <div className="flex flex-wrap gap-5 mt-10 justify-center items-center md:justify-start">
               <div className="flex gap-5 items-center">
-                <SlSocialLinkedin size={30} />
-                <VscGithub size={30} />
+                <a
+                  href="https://linkedin.com/in/thabo-gulu"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <SlSocialLinkedin size={30} />
+                </a>
+
+                <a
+                  href="https://github.com/tgulu"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <VscGithub size={30} />
+                </a>
+
+                {/* <ul>
+                  {links.map(({ id, href, child }) => (
+                    <li key={id} className={"gap-5"}>
+                      <a
+                        href={href}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="flex-row"
+                      >
+                        {child}
+                      </a>
+                    </li>
+                  ))}
+                </ul> */}
+
                 <button className="text-white h-12 w-20 flex justify-around items-center rounded-md bg-[#020617] hover:scale-110 cursor-pointer border-2 border-[#e7e7e7]">
                   <Link to="portfolio" smooth duration={500}>
                     Projects
@@ -36,9 +86,10 @@ const Homes = () => {
                 </button>
               </div>
             </div>
-            <div className="border-4 border-blue-700 mt-4 md:mt-20">
+            <div className="mt-4 md:mt-20">
               <p className="text-center text-2xl">Tech Stack</p>
-              <ul className="mt-3 flex flex-row gap-7 justify-center ">
+
+              <ul className="mt-3 flex flex-row gap-5 md:gap-24 justify-center ">
                 <li className="hover:scale-125 duration-200">
                   <img
                     src="https://skillicons.dev/icons?i=js,kotlin"
@@ -62,7 +113,7 @@ const Homes = () => {
           </div>
 
           {/* Right section: Profile Image */}
-          <div className="md:ml-10 m-0 flex-shrink-0 border-4 border-pink-800 rounded-2xl">
+          <div className="md:ml-10 m-0 flex-shrink-0  rounded-2xl">
             <img
               src={Profile}
               alt="profile pic"
