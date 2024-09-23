fbcd7b59a52cdddb96c616d206e59fcd9debd788
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2024-09-23 20:43:29 +0100

    add more screen sizes and grid temps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,33 @@
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        extend: {},
-        fontFamily: {
-            signature: ["Karla"],
-            style: ["Anton"],
-            jazz: ["Jost"],
-            rise: ["IBM+Plex+Mono"],
-            float: ["Anuphan"],
-            sedgwick: ["Sedgwick Ave Display"],
-            tilt: ["Tilt Prism"],
-            lilita: ["Lilita One"],
-            neue: ["Bebas Neue"],
-        },
-        screens: {
-            sm: "640px",
-            md: "768px",
-            lg: "1024px",
-            xl: "1280px",
-            "2xl": "1536px",
-            "portfolio": { "raw": "(max-width: 640px)" },
-        },
-
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      signature: ["Karla"],
+      style: ["Anton"],
+      jazz: ["Jost"],
+      rise: ["IBM+Plex+Mono"],
+      float: ["Anuphan"],
+      sedgwick: ["Sedgwick Ave Display"],
+      tilt: ["Tilt Prism"],
+      lilita: ["Lilita One"],
+      neue: ["Bebas Neue"],
     },
-    plugins: [],
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      portfolio: { raw: "(max-width: 640px)" },
+    },
+    gridTemplateRows: {
+      // Simple 16 row grid
+      21: "repeat(4, minmax(300px, 1fr))",
+
+      // Complex site-specific row configuration
+      layout: "200px minmax(900px, 1fr) 100px",
+    },
+  },
+  plugins: [],
 };
