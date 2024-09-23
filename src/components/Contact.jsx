d02a2ce73c36b91f00d500ff92f6e872e4f4d611
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2024-09-23 20:41:36 +0100

    change bg color and text to white, move submit button up the page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,19 +4,17 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="bg-gradient-to-b from-black to-gray-800 w-full p-4 text-white md:h-screen"
+      className="bg-gradient-to-b from-white via-[#f9fafb] to-white w-full p-4 text-black   h-screen"
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
         <div className="pb-8">
-          <p className="pt-20 font-float mt-0 mb-4 text-4xl ">
-            {" "}
-            <span className="border-b-2 white">Contact </span>
+          <p className="pt-20 font-float mt-0 mb-4 text-5xl ">
+            <span className="border-b-2 font-jazz">Contact </span>
           </p>
 
           <p className="py-2">
-            Want to learn more about my experience and skills as a junior
-            developer? Fill out the form below to get in touch and start the
-            conversation.
+            Want to learn more about my experience and skills as a developer?
+            Fill out the form below to get in touch and start the conversation.
           </p>
         </div>
         <div className="flex justify-center items-center">
@@ -29,23 +27,23 @@ const Contact = () => {
               type="text"
               name="name"
               placeholder="Enter Your Name"
-              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+              className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
             />
             <input
               type="text"
               name="email"
               placeholder="Enter Your Email"
-              className="my-2 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+              className="my-1 p-2 bg-transparent border-2 rounded-md focus:outline-none"
             />
 
             <textarea
               type="text"
               name="message"
               placeholder="Enter Your Message"
-              rows="10"
-              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+              rows="8"
+              className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
             ></textarea>
-            <button className="text-white bg-gradient-to-b from-cyan-500 to-purple-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+            <button className="bg-gradient-to-b from-slate-700 to-slate-950 px-6 py-3 mt-2  mx-auto flex items-center rounded-md hover:scale-110 duration-300 text-white">
               Submit to Talk
             </button>
           </form>
