8f308f1cf7e75a06b0e8dc6677140e795f7315ff
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2024-09-23 21:07:36 +0100

    remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import About from "./components/About";
 import Contact from "./components/Contact";
-import Experience from "./components/Experience";
-import Home from "./components/Home";
 import Homes from "./components/Homes";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
-import SocialLinks from "./components/SocialLinks";
 
 function App() {
   return (
