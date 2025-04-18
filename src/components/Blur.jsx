cdf87caf0c7b6ea0ddc02703a1c73f1318bf7135
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-04-18 23:51:29 +0100

    change locations for outer blobs

--- src/components/Blur.jsx
+++ src/components/Blur.jsx
@@ -6,8 +6,9 @@ const Blur = () => {
       <div className="relative w-full max-w-lg">
         <div className="absolute top-20 -left-8 md:w-60 md:h-[200px] w-36 h-44 bg-purple-400 rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay6000"></div>
         <div className="absolute top-0 right-8 md:-right-20 md:w-60 md:h-[200px] w-36 h-44 bg-pink-400  rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay2000"></div>
-        <div className="absolute -bottom-8 left-4 md:left-20 md:w-60 md:h-[200px] w-36 h-44 bg-cyan-400 rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay4000"></div>
-        <div className="absolute bottom-28 md:-bottom-32 right-16 md:left-96 md:w-60 md:h-[200px] w-36 h-44 bg-indigo-400 rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay4000"></div>
+        <div className="absolute -bottom-8 left-4 md:-left-36 md:w-60 md:h-[200px] w-36 h-44 bg-cyan-400 rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay6000"></div>
+        <div className="absolute bottom-28 md:-bottom-3 right-2 md:left-32 md:w-60 md:h-[200px] w-36 h-44 bg-indigo-400 rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay4000"></div>
+        <div className="absolute bottom-28 md:-bottom-3 right-16 md:left-[30rem] md:w-60 md:h-[200px] w-36 h-44 bg-blue-600 rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay2000"></div>
       </div>
     </div>
   );
