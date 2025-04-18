3bb7919b22c89cbaa86d1f5b7a0bc63b4e7e26d5
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-04-18 23:33:01 +0100

    add boarder card for logos

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaReact, FaHtml5, FaCss3Alt, FaSass } from "react-icons/fa";
+import { FaReact, FaHtml5, FaCss3Alt } from "react-icons/fa";
 import {
   SiNextdotjs,
   SiTypescript,
@@ -9,24 +9,26 @@ import {
 } from "react-icons/si";
 
 const stack = [
-  { name: "React", icon: <FaReact size={30} /> },
-  { name: "JavaScript", icon: <SiJavascript size={30} /> },
-  { name: "Next JS", icon: <SiNextdotjs size={30} /> },
-  { name: "Tailwind", icon: <SiTailwindcss size={30} /> },
-  { name: "HTML", icon: <FaHtml5 size={30} /> },
-  { name: "CSS", icon: <FaCss3Alt size={30} /> },
+  { name: "React", icon: <FaReact size={30} color="#61DBFB" /> },
+  { name: "JavaScript", icon: <SiJavascript size={30} color="#F7DF1E" /> },
+  { name: "Next JS", icon: <SiNextdotjs size={30} color="#000000" /> },
+  { name: "Tailwind", icon: <SiTailwindcss size={30} color="#38BDF8" /> },
+  { name: "HTML", icon: <FaHtml5 size={30} color="#E34F26" /> },
+  { name: "CSS", icon: <FaCss3Alt size={30} color="#1572B6" /> },
 ];
 
 const TechStack = () => {
   return (
-    <div className="grid grid-rows-3 sm:grid-cols-2 md:grid-cols-3 gap-6">
+    <div className="grid grid-rows-3 sm:grid-cols-2 md:grid-cols-3 gap-6 px-10 m-9">
       {stack.map((tech, index) => (
         <div
           key={index}
-          className=" border border-gray-800 rounded-xl p-4 flex flex-col items-center justify-center hover:border-gray-600 transition"
+          className="border border-black rounded-xl  flex flex-col items-center justify-center hover:border-white/60 bg-white/20 backdrop-blur-md transition duration-300"
         >
-          <div className="mb-2">{tech.icon}</div>
-          <span className="text-sm font-light">{tech.name}</span>
+          <div className="mb-4 p-3 border border-black rounded-full">
+            {tech.icon}
+          </div>
+          <span className="text-sm text-black font-light">{tech.name}</span>
         </div>
       ))}
     </div>
