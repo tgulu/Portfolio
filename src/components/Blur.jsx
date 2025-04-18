5b419421750bfd417e0663cf5a9ea617b3171e44
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-04-18 23:33:52 +0100

    add new distances for blurs

--- src/components/Blur.jsx
+++ src/components/Blur.jsx
@@ -4,10 +4,10 @@ const Blur = () => {
   return (
     <div className="absolute inset-0 -z-10 flex items-center justify-center">
       <div className="relative w-full max-w-lg">
-        <div className="absolute top-0 -left-8 md:w-60 md:h-[250px] w-36 h-44 bg-purple-300 rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay6000"></div>
-        <div className="absolute top-0 -right-4 md:w-60 md:h-[250px] w-36 h-44 bg-cyan-300 rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay2000"></div>
-        <div className="absolute -bottom-8 left-20 md:w-60 md:h-[250px] w-36 h-44 bg-pink-300 rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay4000"></div>
-        <div className="absolute -bottom-8 right-64 md:w-60 md:h-[250px] w-36 h-44 bg-orange-300 rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay4000"></div>
+        <div className="absolute top-20 -left-8 md:w-60 md:h-[200px] w-36 h-44 bg-purple-400 rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay6000"></div>
+        <div className="absolute top-0 right-8 md:-right-20 md:w-60 md:h-[200px] w-36 h-44 bg-pink-400  rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay2000"></div>
+        <div className="absolute -bottom-8 left-4 md:left-20 md:w-60 md:h-[200px] w-36 h-44 bg-cyan-400 rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay4000"></div>
+        <div className="absolute bottom-28 md:-bottom-32 right-16 md:left-96 md:w-60 md:h-[200px] w-36 h-44 bg-indigo-400 rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay4000"></div>
       </div>
     </div>
   );
