48763fe70e05e7da50547423636c5729fcf2eb75
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-04-18 23:31:33 +0100

    add new pages for home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import Contact from "./components/Contact";
 import Homes from "./components/Homes";
+import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
+import Blur from "./components/Blur";
+import Homes2 from "./components/Homes2";
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <Homes />
+    <div className="m-2 px-4 md:px-2">
+      {/* <Blur /> */}
+      <Homes2 />
+      {/* <Homes /> */}
+
+      {/* <NavBar />
+
       <Portfolio />
-      <Contact />
+      <Contact /> */}
     </div>
   );
 }
