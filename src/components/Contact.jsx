98e5afa1aef5314764374d86c790f2c590a69e4b
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-04-19 19:13:52 +0100

    center text for mobile view, change font to same as rest of website

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,52 +1,59 @@
 import React from "react";
+import Blur from "./Blur";
 
 const Contact = () => {
   return (
-    <div
-      name="contact"
-      className="bg-gradient-to-b from-white via-[#f9fafb] to-white w-full p-4 text-black   h-screen"
-    >
-      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
-        <div className="pb-8">
-          <p className="pt-20 font-float mt-0 mb-4 text-5xl ">
-            <span className="border-b-2 font-jazz">Contact </span>
-          </p>
+    <div>
+      <Blur />
+      <div name="contact" className="w-full p-4 text-black h-screen">
+        <Blur />
+        <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+          {/* Heading Section */}
+          <div className="pb-8 text-center">
+            <p className="pt-20  mt-0 mb-2 text-5xl">
+              <span className="border-b-2 ">CONTACT</span>
+            </p>
+            <p className="py-1">
+              Want to collaborate or discuss a project idea? Fill out the form
+              below, and let’s bring your vision to life!
+            </p>
+          </div>
 
-          <p className="py-2">
-            Want to collaborate or discuss a project idea? Fill out the form
-            below, and let’s bring your vision to life!
-          </p>
-        </div>
-        <div className="flex justify-center items-center">
-          <form
-            action="https://getform.io/f/50805f75-578f-493c-90f2-bfcc9510eb3f"
-            method="POST"
-            className="flex flex-col w-full md:w-1/4"
-          >
-            <input
-              type="text"
-              name="name"
-              placeholder="Enter Your Name"
-              className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
-            />
-            <input
-              type="text"
-              name="email"
-              placeholder="Enter Your Email"
-              className="my-1 p-2 bg-transparent border-2 rounded-md focus:outline-none"
-            />
+          {/* Form Section */}
+          <div className="flex justify-center items-center ">
+            <form
+              action="https://getform.io/f/50805f75-578f-493c-90f2-bfcc9510eb3f"
+              method="POST"
+              className="flex flex-col w-full md:w-1/4"
+            >
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter Your Name"
+                className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
+              />
+              <input
+                type="text"
+                name="email"
+                placeholder="Enter Your Email"
+                className="my-1 p-2 bg-transparent border-2 rounded-md focus:outline-none"
+              />
+              <textarea
+                name="message"
+                placeholder="Enter Your Message"
+                rows="8"
+                className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
+              ></textarea>
 
-            <textarea
-              type="text"
-              name="message"
-              placeholder="Enter Your Message"
-              rows="8"
-              className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
-            ></textarea>
-            <button className="bg-gradient-to-b from-slate-700 to-slate-950 px-6 py-3 mt-2  mx-auto flex items-center rounded-md border-2 border-black hover:scale-110 duration-300 text-white">
-              Submit to Talk
-            </button>
-          </form>
+              {/* Submit Button */}
+              <button
+                className="bg-gray-400/20
+              px-6 py-3 mt-2 mx-auto flex items-center rounded-md border-2 border-gray/70 hover:scale-110 duration-300 text-black"
+              >
+                Submit to Talk
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
