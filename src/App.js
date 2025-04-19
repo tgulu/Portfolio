f23f955904791ed300a62e8e80e80827943c1167
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-04-19 19:14:39 +0100

    add contact page back into app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import Contact from "./components/Contact";
-import Homes from "./components/Homes";
-import Home from "./components/Home";
-import NavBar from "./components/NavBar";
-import Portfolio from "./components/Portfolio";
-import Blur from "./components/Blur";
+import Portfolios from "./components/Portfolios";
 import Homes2 from "./components/Homes2";
 
 function App() {
   return (
-    <div className="    px-4 md:px-60 ">
-      {/* <Blur /> */}
+    // Check what div does
+    <div className="px-4 md:px-60 ">
       <Homes2 />
+
+      <Portfolios />
+      <Contact />
+
+      {/* <Portfolio /> */}
       {/* <Homes /> */}
 
       {/* <NavBar />
 
-      <Portfolio />
-      <Contact /> */}
+
+       */}
     </div>
   );
 }
