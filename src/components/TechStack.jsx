1573e2c9cc8c84af44ea2dc8c80685595a69fe04
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-04-19 12:14:55 +0100

    add more tech stack icons

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,27 +1,36 @@
 import React from "react";
-import { FaReact, FaHtml5, FaCss3Alt } from "react-icons/fa";
+import { FaReact, FaHtml5, FaCss3Alt, FaNodeJs } from "react-icons/fa";
 import {
   SiNextdotjs,
-  SiTypescript,
+  //   SiTypescript,
+  SiJest,
   SiTailwindcss,
-  SiCypress,
   SiJavascript,
 } from "react-icons/si";
 
 const stack = [
-  { name: "React", icon: <FaReact size={25} color="#61DBFB" /> },
+  // Scripting
   { name: "JavaScript", icon: <SiJavascript size={25} color="#F7DF1E" /> },
+  // { name: "TypeScript", icon: <SiTypescript size={25} color="#3178C6" /> },
+  // Core frontend
+  { name: "React", icon: <FaReact size={25} color="#61DBFB" /> },
   { name: "Next JS", icon: <SiNextdotjs size={25} color="#000000" /> },
+
+  // Styling
+  { name: "HTML", icon: <FaHtml5 size={25} color="#E34F26" /> },
+  { name: "CSS", icon: <FaCss3Alt size={25} color="#1572B6" /> },
   { name: "Tailwind", icon: <SiTailwindcss size={25} color="#38BDF8" /> },
-  { name: "HTML", icon: <FaHtml5 size={25} color="#E34F26" /> },
-  { name: "CSS", icon: <FaCss3Alt size={25} color="#1572B6" /> },
-  { name: "HTML", icon: <FaHtml5 size={25} color="#E34F26" /> },
-  { name: "CSS", icon: <FaCss3Alt size={25} color="#1572B6" /> },
+
+  // Backend
+  { name: "Node.js", icon: <FaNodeJs size={25} color="#3C873A" /> },
+
+  // Testing
+  { name: "Jest", icon: <SiJest size={25} color="#99425b" /> },
 ];
 
 const TechStack = () => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-x-16 gap-y-9 justify-items-center ">
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-x-10 gap-y-2 justify-items-center ">
       {stack.map((tech, index) => (
         <div
           key={index}
