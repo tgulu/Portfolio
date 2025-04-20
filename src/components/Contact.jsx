cbf4d6002241957eed42918978cd1b8b6b01c128
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-04-20 11:15:02 +0100

    move blur to be bground colour, make form design mobile responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,17 +3,19 @@ import Blur from "./Blur";
 
 const Contact = () => {
   return (
-    <div>
+    <div
+      name="contact"
+      className="relative min-h-screen flex flex-col items-center justify-center px-4 py-5"
+    >
       <Blur />
-      <div name="contact" className="w-full p-4 text-black h-screen">
-        <Blur />
-        <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+      <div className="w-full p-1 text-black h-screen  ">
+        <div className="flex flex-col p-4 justify-center max-w-screen-lg gap-y-8 mx-auto h-full">
           {/* Heading Section */}
-          <div className="pb-8 text-center">
-            <p className="pt-20  mt-0 mb-2 text-5xl">
+          <div className="pb-0 md:px-28 text-center">
+            <p className="pt-14 md:pt-0 mt-0 mb-2 text-5xl">
               <span className="border-b-2 ">CONTACT</span>
             </p>
-            <p className="py-1">
+            <p className="py-4">
               Want to collaborate or discuss a project idea? Fill out the form
               below, and let’s bring your vision to life!
             </p>
@@ -24,31 +26,31 @@ const Contact = () => {
             <form
               action="https://getform.io/f/50805f75-578f-493c-90f2-bfcc9510eb3f"
               method="POST"
-              className="flex flex-col w-full md:w-1/4"
+              className="flex flex-col w-full md:w-[400px] text-black"
             >
               <input
                 type="text"
                 name="name"
                 placeholder="Enter Your Name"
-                className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
+                className="p-2 bg-transparent border-2 border-gray-600/20 rounded-md focus:outline-none"
               />
               <input
                 type="text"
                 name="email"
                 placeholder="Enter Your Email"
-                className="my-1 p-2 bg-transparent border-2 rounded-md focus:outline-none"
+                className="my-1 p-2 bg-transparent border-2 border-gray-600/20 rounded-md focus:outline-none"
               />
               <textarea
                 name="message"
                 placeholder="Enter Your Message"
                 rows="8"
-                className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
+                className="p-2 bg-transparent border-2 border-gray-600/20  rounded-md focus:outline-none"
               ></textarea>
 
               {/* Submit Button */}
               <button
-                className="bg-gray-400/20
-              px-6 py-3 mt-2 mx-auto flex items-center rounded-md border-2 border-gray/70 hover:scale-110 duration-300 text-black"
+                className="bg-gray-200/10
+              px-6 py-2 mt-2 mx-auto flex items-center rounded-md border-2 border-gray-600/20 hover:scale-110 duration-300 text-black"
               >
                 Submit to Talk
               </button>
