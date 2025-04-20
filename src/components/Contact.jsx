775c80c22e7e30e1a8adf19311f2d5d612ace599
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-04-20 16:20:29 +0100

    remove padding top for whole contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="relative min-h-screen flex flex-col items-center justify-center px-4 py-5"
+      className="relative min-h-screen flex flex-col items-center justify-center px-4 "
     >
       <Blur />
       <div className="w-full p-1 text-black h-screen  ">
@@ -13,44 +13,44 @@ const Contact = () => {
           {/* Heading Section */}
           <div className="pb-0 md:px-28 text-center">
             <p className="pt-14 md:pt-0 mt-0 mb-2 text-5xl">
-              <span className="border-b-2 ">CONTACT</span>
+              <span className="border-b-2 border-black ">CONTACT</span>
             </p>
-            <p className="py-4">
+            <p className=" pt-4">
               Want to collaborate or discuss a project idea? Fill out the form
               below, and let’s bring your vision to life!
             </p>
           </div>
 
           {/* Form Section */}
-          <div className="flex justify-center items-center ">
+          <div className="flex justify-center items-center  ">
             <form
               action="https://getform.io/f/50805f75-578f-493c-90f2-bfcc9510eb3f"
               method="POST"
-              className="flex flex-col w-full md:w-[400px] text-black"
+              className="flex flex-col w-full md:w-[400px] placeholder-black "
             >
               <input
                 type="text"
                 name="name"
                 placeholder="Enter Your Name"
-                className="p-2 bg-transparent border-2 border-gray-600/20 rounded-md focus:outline-none"
+                className="p-2 bg-transparent border-2 hover:border-gray-600/20 border-black placeholder-black rounded-md focus:outline-none"
               />
               <input
                 type="text"
                 name="email"
                 placeholder="Enter Your Email"
-                className="my-1 p-2 bg-transparent border-2 border-gray-600/20 rounded-md focus:outline-none"
+                className="my-1 p-2 bg-transparent border-2 hover:border-gray-600/20 border-black rounded-md placeholder-black focus:outline-none"
               />
               <textarea
                 name="message"
                 placeholder="Enter Your Message"
                 rows="8"
-                className="p-2 bg-transparent border-2 border-gray-600/20  rounded-md focus:outline-none"
+                className="p-2 bg-transparent border-2 hover:border-gray-600/20 border-black rounded-md focus:outline-none placeholder-black"
               ></textarea>
 
               {/* Submit Button */}
               <button
                 className="bg-gray-200/10
-              px-6 py-2 mt-2 mx-auto flex items-center rounded-md border-2 border-gray-600/20 hover:scale-110 duration-300 text-black"
+              px-6 py-2 mt-2 mx-auto flex items-center rounded-md border-2 hover:border-gray-600/20  border-black hover:scale-110 duration-300 text-black"
               >
                 Submit to Talk
               </button>
