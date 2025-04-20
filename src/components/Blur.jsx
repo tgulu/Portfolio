84422da6db144c15d5bd5eee6c4531b7599d2953
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-04-20 18:20:15 +0100

    change speed and location of blobs

--- src/components/Blur.jsx
+++ src/components/Blur.jsx
@@ -1,29 +1,23 @@
 import React from "react";
 
 const Blur = () => {
-  {
-    /* TO DO */
-  }
-  {
-    /* Create a randomizer to pick the colors for the blobs in each page section  */
-  }
-
   return (
-    <div className="absolute inset-0 -z-10 flex items-center justify-center">
-      <div className="relative w-full max-w-lg">
-        <div className="absolute top-8 -left-8 md:w-60 md:h-[180px] w-36 h-44 bg-purple-400 rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay6000"></div>
+    <div className="fixed inset-0 -z-10 overflow-hidden">
+      <div className="relative w-full h-full">
+        {/* Top Left */}
+        <div className="absolute top-[-10%] left-[-10%] w-[40vw] h-[40vw] md:w-[30vw] md:h-[30vw] lg:w-[25vw] lg:h-[25vw] bg-purple-400 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blobScaleDelay2000" />
 
-        <div className="absolute top-0 right-8 md:-right-20 md:w-60 md:h-[200px] w-36 h-44 bg-pink-400  rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay2000"></div>
+        {/* Top Right */}
+        <div className="absolute top-[-15%] right-[-10%] w-[40vw] h-[40vw] md:w-[30vw] md:h-[30vw] lg:w-[25vw] lg:h-[25vw] bg-pink-400 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blobScaleDelay4000" />
 
-        {/* TO DO */}
-        {/* Increase size of blurs for full screens,  */}
-        <div className="absolute -bottom-8 left-4 md:-left-36 md:w-60 md:h-[200px] w-36 h-44 bg-cyan-400 rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay6000"></div>
+        {/* Bottom Left */}
+        <div className="absolute bottom-[-10%] left-[-10%] w-[40vw] h-[40vw] md:w-[30vw] md:h-[30vw] lg:w-[25vw] lg:h-[25vw] bg-cyan-400 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blobScaleDelay6000" />
 
-        <div className="absolute bottom-28 md:-bottom-3 right-2 md:left-32 md:w-60 md:h-[200px] w-36 h-44 bg-indigo-400 rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay4000"></div>
+        {/* Bottom Right */}
+        <div className="absolute bottom-[-10%] right-[-10%] w-[40vw] h-[40vw] md:w-[30vw] md:h-[30vw] lg:w-[25vw] lg:h-[25vw] bg-indigo-400 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blobScaleDelay2000" />
 
-        <div className="absolute bottom-28 md:-bottom-3 right-16 md:left-[30rem] md:w-60 md:h-[200px] w-36 h-44 bg-blue-600 rounded-full mix-blend-multiply filter blur-2xl opacity-80 animate-blobScaleDelay2000"></div>
-
-        {/* <div className="absolute top-64 md:-bottom-3 right-16 md:left-[30rem] md:w-60 md:h-[100px] w-36 h-44 bg-purple-600 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blobScaleDelay2000"></div> */}
+        {/* Center Behind Content */}
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[80vw] h-[80vw] md:w-[50vw] md:h-[50vw] lg:w-[40vw] lg:h-[40vw] bg-blue-500 rounded-full mix-blend-multiply filter blur-[140px] opacity-40 animate-blobScaleDelay4000" />
       </div>
     </div>
   );
