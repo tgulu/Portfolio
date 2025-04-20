ffbb1cecec32d84d98cd32ff7ad9dc1dc015c1ca
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2025-04-20 18:35:45 +0100

    add center pink blob that doesnt overflow over pages

--- src/components/Blur.jsx
+++ src/components/Blur.jsx
@@ -8,7 +8,7 @@ const Blur = () => {
         <div className="absolute top-[-10%] left-[-10%] w-[40vw] h-[40vw] md:w-[30vw] md:h-[30vw] lg:w-[25vw] lg:h-[25vw] bg-purple-400 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blobScaleDelay2000" />
 
         {/* Top Right */}
-        <div className="absolute top-[-15%] right-[-10%] w-[40vw] h-[40vw] md:w-[30vw] md:h-[30vw] lg:w-[25vw] lg:h-[25vw] bg-pink-400 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blobScaleDelay4000" />
+        <div className="absolute top-[-15%] right-[-10%] w-[40vw] h-[40vw] md:w-[30vw] md:h-[30vw] lg:w-[25vw] lg:h-[25vw] bg-pink-600 rounded-full mix-blend-multiply filter blur-3xl opacity-80 animate-blobScaleDelay4000" />
 
         {/* Bottom Left */}
         <div className="absolute bottom-[-10%] left-[-10%] w-[40vw] h-[40vw] md:w-[30vw] md:h-[30vw] lg:w-[25vw] lg:h-[25vw] bg-cyan-400 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blobScaleDelay6000" />
@@ -17,7 +17,7 @@ const Blur = () => {
         <div className="absolute bottom-[-10%] right-[-10%] w-[40vw] h-[40vw] md:w-[30vw] md:h-[30vw] lg:w-[25vw] lg:h-[25vw] bg-indigo-400 rounded-full mix-blend-multiply filter blur-3xl opacity-70 animate-blobScaleDelay2000" />
 
         {/* Center Behind Content */}
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[80vw] h-[80vw] md:w-[50vw] md:h-[50vw] lg:w-[40vw] lg:h-[40vw] bg-blue-500 rounded-full mix-blend-multiply filter blur-[140px] opacity-40 animate-blobScaleDelay4000" />
+        <div className="absolute top-1/4 left-1/4 -translate-x-[-90%] -translate-y-20 w-[80vw] h-[80vw] md:w-[60vw] md:h-[50vw] lg:w-[40vw] lg:h-[40vw] bg-pink-400 rounded-full mix-blend-multiply filter blur-[140px] opacity-60 animate-blobScaleDelay4000" />
       </div>
     </div>
   );
